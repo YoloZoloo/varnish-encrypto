2eb21682d596970c87f6815b38de77ef5829f7be
YoloZoloo <zolozooro@gmail.com>
2022-07-13 10:11:49 +0900

    fix: change client TLS method

--- src/functions.hpp
+++ src/functions.hpp
@@ -110,7 +110,7 @@ SSL_CTX *InitCTX(void)
     SSL_CTX *ctx;
     OpenSSL_add_all_algorithms();     /* Load cryptos, et.al. */
     SSL_load_error_strings();         /* Bring in and register error messages */
-    method = TLSv1_2_client_method(); /* Create new client-method instance */
+    method = TLS_client_method(); /* Create new client-method instance */
     ctx = SSL_CTX_new(method);        /* Create new context */
     if (ctx == NULL)
     {
